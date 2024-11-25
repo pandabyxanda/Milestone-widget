4fcc20a2f75b2586941c0caaffe8fad5e3709b11
Pandabyxanda <edjjxx1@gmail.com>
2024-11-25 20:42:51 +0800

    refactor: set hardcoded text as string res. Update desc

--- app/src/main/java/com/example/milestone_widget/other_pages/ItemPageCreate.kt
+++ app/src/main/java/com/example/milestone_widget/other_pages/ItemPageCreate.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -67,7 +68,7 @@ fun ItemPageCreate(
         Column {
             TopBarItemPage(
                 navController = navController,
-                text = "Create new item"
+                text = stringResource(R.string.create_new_item)
             )
             Spacer(modifier = Modifier.height(16.dp))
             TextFieldWithPlaceholder(
